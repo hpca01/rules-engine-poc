adbb00d1a139a3bbbe8075c930aba3714b902c8d
Hiren <hirenpatelca@gmail.com>
2025-06-28 10:40:45 -0700

    - updated gitignore - added env files - updated docker-compose for the db - created db initialization as an entry event requirement for web component
    todo:
    - work on wiring up api with get/post methods
    - add a queue
    - add tests for API in general

--- ingress/main.py
+++ ingress/main.py
@@ -1,11 +1,28 @@
 from typing import Union
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
+from models import init_db, get_session
+from contextlib import asynccontextmanager
+from functools import partial
+from sqlmodel import Session
 
-app = FastAPI()
+engine = None
+session = partial(get_session, engine)
+
+@asynccontextmanager
+async def init_db_resources(app: FastAPI):
+    global session
+    global engine
+    engine = init_db()
+    session = partial(get_session, engine)
+    yield
+
+app = FastAPI(lifespan=init_db_resources)
 
 @app.post("/new-event")
-async def new_event(request:Request):
+async def new_event(request:Request, session:Session = Depends(session)):
+    #Insert payload into pgsql and queue for processing
+    #return ID of insert
     json_payload = await request.json()
     return {"Hello": "World"}
 
@@ -14,5 +31,6 @@ async def health(request:Request):
     return {"status": "Active"}
 
 @app.get("/status/{item_id}")
-async def status(item_id: int, q: Union[str, None] = None):
+async def status(item_id: int, q: Union[str, None] = None, session:Session = Depends(session)):
+    #query pgsql by id using sqlmodel
     return {"item_id": item_id, "q": q}
--- ingress/models.py
+++ ingress/models.py
@@ -2,8 +2,10 @@ from typing import Annotated, Optional, Dict
 from datetime import datetime
 
 from sqlmodel import Field, Session, SQLModel, create_engine, select, TIMESTAMP
+from sqlalchemy.engine import Engine
 from sqlalchemy import Column, DateTime, func
 from sqlalchemy.dialects.postgresql import JSON
+import os
 
 
 class Event(SQLModel, table=True):
@@ -12,3 +14,21 @@ class Event(SQLModel, table=True):
                                           sa_column_kwargs={'server_default': func.now()}, nullable=False)
     modified_at:Optional[datetime] = Field(default=None, sa_type=DateTime(timezone=True), sa_column_kwargs={'onupdate': func.now(), 'server_default':func.now()})
     event:Dict[str,str] = Field(sa_type=JSON, nullable=False)
+
+engine = None 
+
+def init_db():
+    #TODO: Remove this as a global
+    global engine
+    user=os.environ.get("POSTGRES_USER")
+    dbname=os.environ.get("POSTGRES_DB")
+    password = os.environ.get("POSTGRES_PASSWORD")
+    print(f'user {user} dbname {dbname} password {password} db')
+    engine = create_engine(f"postgresql://{user}:{password}@db/{dbname}", echo=True)
+    SQLModel.metadata.create_all(engine)
+    return engine
+
+
+def get_session(engine: Engine):
+    with Session(engine) as session:
+        yield session
