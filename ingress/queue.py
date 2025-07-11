153fc5817c2c42dfaec772113644d4f8af82aa71
hpca01 <hirenpatelca@gmail.com>
2025-07-11 07:28:12 -0700

    added publisher class for ingress

--- ingress/queue.py
+++ ingress/queue.py
@@ -1,6 +1,22 @@
 import asyncio
+from typing import Any
 
 import nats
+from nats.errors import NoServersError
 
-def connect():
-    pass
+
+class Publisher:
+    def __init__(self, *args, **kwargs):
+        self.url = kwargs.get('url')
+        self.user = kwargs.get('user')
+        self.password = kwargs.get('password')
+        self.port = kwargs.get('port')
+        if None in [self.url, self.user, self.password, self.port]:
+            raise NoServersError('Your server setup is invalid need all of (url, user, pass, port)')
+    
+    async def connect(self):
+        self.nc = await nats.connect(f"nats://{self.user}:{self.password}@{self.url}:{self.port}")
+        return self
+
+    def publish(self, topic:str, msg:Any):
+        pass
