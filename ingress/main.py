70534f9b71d7b3024c254ac92cb411c40dab3394
Hiren <hirenpatelca@gmail.com>
2025-07-12 09:42:01 -0700

    - finished queue class - incorporated it into the api for incoming events - todo: need to work on the consumer

--- ingress/main.py
+++ ingress/main.py
@@ -2,45 +2,67 @@ from typing import Union, Optional
 
 from fastapi import FastAPI, HTTPException, Request, Depends
 from .models import init_db, get_session, Event, EventAccepted
+from .queue import Publisher, init_pub
 from contextlib import asynccontextmanager
 from functools import partial
 from sqlmodel import Session, select
 from sqlalchemy.engine import Engine
+import json
 
-engine:Engine = None
+engine: Engine = None
 session = partial(get_session, engine)
+queue = init_pub
+
 
 @asynccontextmanager
-async def init_db_resources(app: FastAPI):
+async def init_resources(app: FastAPI):
     global session
     global engine
     engine = init_db()
     session = partial(get_session, engine)
+    queue = init_pub()
     yield
-    #close on program exit
+    # close resources on program exit
     session.__exit__()
+    await queue.close()
     engine.dispose(True)
 
-app = FastAPI(lifespan=init_db_resources)
+
+app = FastAPI(lifespan=init_resources)
+
 
 @app.post("/new_event")
-async def new_event(request:Request, session:Session = Depends(session))->EventAccepted:
-    #Insert payload into pgsql and queue for processing
-    #return ID of insert
+async def new_event(
+    request: Request,
+    session: Session = Depends(session),
+    queue: Publisher = Depends(queue),
+) -> EventAccepted:
+    # Insert payload into pgsql and queue for processing
+    # return ID of insert
     json_payload = await request.json()
+    obj = json.loads(json_payload)
     event = Event(event=json_payload)
     session.add(event)
     session.commit()
     assert event.id is not None, "Event ID is null, it is not commited"
+    await queue.publish(
+        obj["location"] if obj["location"] else "new",
+        json_payload,
+        obj["headers"] if obj["headers"] else None,
+    )
     return EventAccepted(event_id=event.id)
 
+
 @app.get("/health")
-async def health(request:Request):
+async def health(request: Request):
     return {"status": "Active"}
 
+
 @app.get("/status/{event_id}", response_model=Event)
-async def status(event_id: int, q: Union[str, None] = None, session:Session = Depends(session)):
-    #query pgsql by id using sqlmodel
+async def status(
+    event_id: int, q: Union[str, None] = None, session: Session = Depends(session)
+):
+    # query pgsql by id using sqlmodel
     event = session.exec(select(Event, event_id))
     if not event:
         raise HTTPException(status_code=404, detail="Event not found")
--- ingress/queue.py
+++ ingress/queue.py
@@ -1,5 +1,6 @@
 import asyncio
-from typing import Any
+from typing import Any, Dict, Optional
+import os
 
 import nats
 from nats.errors import NoServersError
@@ -7,16 +8,36 @@ from nats.errors import NoServersError
 
 class Publisher:
     def __init__(self, *args, **kwargs):
-        self.url = kwargs.get('url')
-        self.user = kwargs.get('user')
-        self.password = kwargs.get('password')
-        self.port = kwargs.get('port')
+        self.url = kwargs.get("url")
+        self.user = kwargs.get("user")
+        self.password = kwargs.get("password")
+        self.port = kwargs.get("port")
         if None in [self.url, self.user, self.password, self.port]:
-            raise NoServersError('Your server setup is invalid need all of (url, user, pass, port)')
-    
+            raise NoServersError(
+                "Your server setup is invalid need all of (url, user, pass, port)"
+            )
+
     async def connect(self):
-        self.nc = await nats.connect(f"nats://{self.user}:{self.password}@{self.url}:{self.port}")
+        self.nc = await nats.connect(
+            f"nats://{self.user}:{self.password}@{self.url}:{self.port}"
+        )
         return self
 
-    def publish(self, topic:str, msg:Any):
-        pass
+    async def publish(self, topic: str, msg: str, headers: Optional[Dict[str, str]]):
+        if not hasattr(self, "nc"):
+            raise NoServersError("You have not connected to the server")
+        else:
+            return await self.nc.publish(
+                topic, bytes(msg), None, headers if headers else None
+            )
+
+    async def close(self):
+        await self.nc.close()
+
+
+def init_pub() -> Publisher:
+    url = os.environ.get("NATS_URL")
+    user = os.environ.get("NATS_URL")
+    password = os.environ.get("NATS_PASS")
+    port = os.environ.get("NATS_PORT")
+    return Publisher(url=url, user=user, password=password, port=port)
